9211eff85e7daa5b436f2ef1c35e7d6ec14084a0
Gopal jha <145859112+Gopaljha2004@users.noreply.github.com>
2024-09-21 04:39:38 +0530

    Delete Backend testing directory

--- Backend testing/routes/signup.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const SingupController = require("../controller/SingupController")
-
-router.post('/signup', SingupController);
-
-module.exports = router;
