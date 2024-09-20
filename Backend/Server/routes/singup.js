c37eaaf773ebe5d638fb4ba6602236cbddd8811a
Gopal jha <145859112+Gopaljha2004@users.noreply.github.com>
2024-09-21 04:40:21 +0530

    Delete Backend/Server directory

--- Backend/Server/routes/singup.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example controller function (you might be missing this or have incorrect syntax)
-const {Singup} = require("../controllers/Singup");
-// Define your route with the correct callback function
-router.post("/sigup" , Singup)
-
-module.exports = router;
